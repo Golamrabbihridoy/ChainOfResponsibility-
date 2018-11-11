cdad456f405db58bda4a302d378a2bc4597984c3
Golamrabbihridoy <37367955+Golamrabbihridoy@users.noreply.github.com>
2018-11-11 21:29:59 +0600

    Update Test.java

--- Test.java
+++ Test.java
@@ -14,10 +14,10 @@ class Test {
 		   		chaincalc1.setNextChain(chaincalc1);
 		   		chaincalc2.setNextChain(chaincalc2);
 		   		chaincalc3.setNextChain(chaincalc3);
-		   		assertEquals("5.0",chaincalc1.calculate(new Numbers(3.0,2.0,"add")));
-		   		assertEquals("2.0",chaincalc1.calculate(new Numbers(4.0,2.0,"sub")));
-		   		assertEquals("8.0",chaincalc1.calculate(new Numbers(4.0,2.0,"mult")));
-		   		assertEquals("3.0",chaincalc1.calculate(new Numbers(6.0,2.0,"div")));
+		   		assertEquals(5.0,chaincalc1.calculate(new Numbers(3.0,2.0,"add")));
+		   		assertEquals(2.0,chaincalc2.calculate(new Numbers(4.0,2.0,"sub")));
+		   		assertEquals(8.0,chaincalc3.calculate(new Numbers(4.0,2.0,"mult")));
+		   		assertEquals(3.0,chaincalc4.calculate(new Numbers(6.0,2.0,"div")));
 		       }
 		
 	}
